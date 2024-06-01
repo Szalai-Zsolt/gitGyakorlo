115ed325c8213f83719d14a5d18eccb3e278c431
Szalai <tlosz9@gmail.com>
2024-06-01 10:01:45 +0200

    prog tetel 1-4

--- src/main/GitGyakorlas.java
+++ src/main/GitGyakorlas.java
@@ -4,29 +4,63 @@ package main;
 
 public class GitGyakorlas {
 
-   
-    public static void main(String[] args) {
+   private static int[] sorozat=new int[5];
+    
+   public static void main(String[] args) {
        feladatok();
     }
 
     private static void feladatok() {
-       
+       tombFeltoltes();
+       osszegzes();
+       megszamlalas();
+       minKivalaszt();
     }
 
     private static int osszegzes(){
-        return 0;
+       int osszeg = 0;
+        
+       for (int i = 0; i < sorozat.length; i++) {
+            osszeg += sorozat[i];
+        }
+       
+       konzolraIr("a sorozat Összege: " + osszeg + "\n");
+       return osszeg;
     }
     
     private static int megszamlalas(){
-        return 0;
+        int db = 0;
+        for (int i = 0; i < sorozat.length; i++) {
+            if (sorozat[i] % 2 == 0) {
+                db++; 
+            }
+        }
+        konzolraIr("A páros számok darabszáma: " + db + "\n");
+        return db;
     }
     
+    
     private static int minKivalaszt(){
-        return 0;
+        int minIndex = 0;
+        for (int i = 1; i < sorozat.length; i++) {
+            if(sorozat[i] < sorozat[minIndex]){
+                minIndex = i;
+            }
+        }
+        konzolraIr("A legkisebb érték indexe: " + minIndex + "\n");
+        return minIndex;
     }
     
     private static int maxKivalaszt(){
-        return 0;
+        int maxIndex = 0;
+        for (int i = 1; i < sorozat.length; i++) {
+           
+            if(sorozat[i] > sorozat[maxIndex]){
+                maxIndex = i;
+            }
+        }
+        konzolraIr("A legnagyobb érték indexe: " + maxIndex + "\n");
+        return maxIndex;
     }
     
     private static int kivalasztas(){
@@ -43,6 +77,13 @@ public class GitGyakorlas {
     
     private static int linearisKereses(){
         return -1;
+    }
+    
+    static void tombFeltoltes(){
+        
+        for (int i = 0; i < sorozat.length; i++) {
+            sorozat[i] = (int)(Math.random()*12) + 9;
+        }
     }
     
     private static void konzolraIr(String kimenet) {
