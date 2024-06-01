d3b2a76a95d51f60e87e8c943a35b88c8eeba715
Szalai <tlosz9@gmail.com>
2024-06-01 09:05:23 +0200

    void feladatok()

--- src/main/GitGyakorlas.java
+++ src/main/GitGyakorlas.java
@@ -6,7 +6,11 @@ public class GitGyakorlas {
 
    
     public static void main(String[] args) {
-       
+       feladatok();
+    }
+
+    private static void feladatok() {
+        
     }
     
 }
