e6fc57b0299b24a51b7f831ca5348ac87012e0bf
Szalai <tlosz9@gmail.com>
2024-06-01 09:22:08 +0200

    prog tetelek elkeszitese

--- src/main/GitGyakorlas.java
+++ src/main/GitGyakorlas.java
@@ -10,9 +10,41 @@ public class GitGyakorlas {
     }
 
     private static void feladatok() {
-        konzolraIr("");
+       
     }
 
+    private static int osszegzes(){
+        return 0;
+    }
+    
+    private static int megszamlalas(){
+        return 0;
+    }
+    
+    private static int minKivalaszt(){
+        return 0;
+    }
+    
+    private static int maxKivalaszt(){
+        return 0;
+    }
+    
+    private static int kivalasztas(){
+        return 0;
+    }
+    
+    private static boolean eldontes_LegalabbEgy(){
+        return false;
+    }
+    
+    private static boolean eldontes_Osszes(){
+        return false;
+    }
+    
+    private static int linearisKereses(){
+        return -1;
+    }
+    
     private static void konzolraIr(String kimenet) {
         System.out.println(kimenet);    
     }
